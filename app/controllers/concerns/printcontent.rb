9ed4a556ff65bb8d25523a73ae0bc3e90053b710
Andrew Burton <abauckland@gmail.com>
2014-12-16 12:45:50 +0000

    place contents in bounding box

--- app/controllers/concerns/printcontent.rb
+++ app/controllers/concerns/printcontent.rb
@@ -64,8 +64,11 @@ module Printcontent
     
     pdf.go_to_page(3)
 
-    page_title_header(pdf)    
-    pdf.table(document_content, :cell_style => contents_style, :column_widths => [455, 35]) unless document_content.blank?
+    pdf.bounding_box([0, 20.mm], :width => 492, :height => 260) do
+      page_title_header(pdf)    
+      pdf.table(document_content, :cell_style => contents_style, :column_widths => [455, 35]) unless document_content.blank?
+    end
+
   end
 
   def page_title_header(pdf)
