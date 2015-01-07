ba209095bb3dc82a9fa018fbf63392dd71a9e9d2
Andrew Burton <abauckland@gmail.com>
2015-01-07 14:32:20 +0000

    csv download added cadimage corrections

--- app/controllers/keynotes_controller.rb
+++ app/controllers/keynotes_controller.rb
@@ -1,5 +1,5 @@
 class KeynotesController < ApplicationController
-  before_filter :authenticate
+#  before_filter :authenticate
   before_action :set_project, only: [:show, :keynote_export]
   before_action :set_revision, only: [:show]
 
@@ -13,22 +13,54 @@ class KeynotesController < ApplicationController
 
 
   def keynote_export    
+      if params[:cad_product] == 'csv'
+        csv_keynote(@project)
+      end 
+
       if params[:cad_product] == 'revit'
-        revit_keynote_export(@project)
+        revit_keynote(@project)
       end 
 
       if params[:cad_product] == 'bentley'
-        bentley_keynote_export(@project)      
+        bentley_keynote(@project)      
       end 
 
       if params[:cad_product] == 'cadimage'
-        cadimage_keynote_export(@project)      
+        cadimage_keynote(@project)      
       end              
   end
+
   
+  def csv_keynote(project)
+  
+    #set up for columns, unique identifier column to be set to 'Create Unique IDS'    
+    headers = ['ref', 'title', 'ref & title']
 
-  def revit_keynote_export(project)
+    @csv_keynote = CSV.generate do |csv|
 
+      csv << headers
+      
+      set_subsections(project)
+      @subsections.each_with_index do |subsection, n|
+        #project subsection
+        csv << [subsection.full_code, subsection.text]
+
+        #for each clause
+        set_clauses(project, subsection)
+        @clauses.each_with_index do |clause, m|
+          #project clauses
+          csv << [clause.caws_code, clause.clausetitle.text, clause.caws_full_title]
+        end
+      end
+    end
+
+    filename = "#{@project.code}_clause_schedule.csv"
+
+    send_data @csv_keynote, filename: filename, :type => 'text/csv'
+  end
+
+
+  def revit_keynote(project)
 
     section = Cawssection.first
 
@@ -36,20 +68,20 @@ class KeynotesController < ApplicationController
 
     project = Project.where(:id => params[:id]).first 
 
-      #for each section 
-      sections = Cawssection.project_sections(project)                                   
-      sections.each_with_index do |s, i|
+      set_sections(project)
+      @sections.each_with_index do |s, i|
         #project section    
         data = data + "#{s.ref}\t#{s.text}\n"       
-        #for each subsection     
-        subsections = Cawssubsection.section_subsections(project, s)               
-        subsections.each_with_index do |subsection, n|
-          #project subsection  
+        #for each subsection
+        set_section_subsections(project, s)
+        @subsections.each_with_index do |subsection, n|
+
+          #project subsection s
           data = data + "#{sprintf("%02d", subsection.ref).to_s}\t#{subsection.text}\t#{subsection.cawssection.ref}\n"
           
-          clauses = Clause.subsection_clauses(project, subsection)                 
-          clauses.each_with_index do |clause, m|
-            #project clauses 
+          set_clauses(project, subsection)
+          @clauses.each_with_index do |clause, m|
+            #project clauses
             data = data + "#{clause.caws_code}\t#{clause.clausetitle.text}\t#{clause.clauseref.subsection.cawssubsection.full_code}\n"
           end 
         end 
@@ -88,68 +120,103 @@ class KeynotesController < ApplicationController
 #  end
 
 
-  def bentley_keynote_export(project)
+#  def bentley_keynote(project)
     
-    filename = project.code + " specright_keynote"   
-    @bim_bentley_export = CSV.generate do |csv|  
+#    filename = project.code + " specright_keynote"   
+#    @bim_bentley_export = CSV.generate do |csv|  
       
       #for each section
-      sections = Cawssection.project_sections(project)
-      sections.each_with_index do |section, i|
+#      sections = Cawssection.project_sections(project)
+#      sections.each_with_index do |section, i|
         #project section
-        csv << [section.ref << ' ' << section.text]
+#        csv << [section.ref << ' ' << section.text]
         
         #for each subsection
-        subsections = Cawssubsection.section_subsections(project, section) 
-        subsections.each_with_index do |subsection, n|
+#        subsections = Cawssubsection.section_subsections(project, section) 
+#        subsections.each_with_index do |subsection, n|
           #project subsection
-          csv << [subsection.subsection_code << '*' << subsection.text]
+#          csv << [subsection.full_code << '*' << subsection.text]
           
           #for each clause
-          clauses = Clause.subsection_clauses(project, subsection) 
-          clauses.each_with_index do |clause, m|          
+#          clauses = Clause.subsection_clauses(project, subsection) 
+#          clauses.each_with_index do |clause, m|          
             #project clauses
-            csv << [clause.clause_code << '*' << clause.clausetitle.text]
-          end  
-        end
-        csv << []        
-      end
-    end  
-    send_data @bim_bentley_export, :type => 'text/plain', :disposition => 'attachment; filename=#{filename}.spc'      
-  end
+#            csv << [clause.caws_code << '*' << clause.clausetitle.text]
+#          end  
+#        end
+#        csv << []        
+#      end
+#    end  
+#    send_data @bim_bentley_export, :type => 'text/plain', :disposition => 'attachment; filename=#{filename}.spc'      
+#  end
 
 
   
-  def cadimage_keynote_export(project)
-    
-    filename = project.code + " specright_keynote"   
-    @cadimage_keynote_export = CSV.generate(:col_sep => "\t") do |csv|
-      
-      #headers
-      #set up for columns, unique identifier column to be set to 'Create Unique IDS'
-      csv << ['category', 'ignore', 'Keynote Key', 'Short Description', 'Long Description', 'Specfication Ref.', 'Time Stamp']  
-      
-      #for each section
-      subsections = Cawssubsection.all_subsections(project)       
-      subsections.each_with_index do |subsection, n|
+  def cadimage_keynote(project)
+  
+    #set up for columns, unique identifier column to be set to 'Create Unique IDS'    
+    headers = ['category', 'ignore', 'Keynote Key', 'Short Description', 'Long Description', 'Specfication Ref.', 'Time Stamp']
+
+    @cadimage_keynote = CSV.generate do |csv|
+
+      csv << headers
+
+      set_subsections(project)
+      @subsections.each_with_index do |subsection, n|
         #project subsection
         csv << [subsection.full_code, '', subsection.full_code, subsection.text]
-        
+
         #for each clause
-        clauses = Clause.subsection_clauses(project, subsection) 
-        clauses.each_with_index do |clause, m|           
+        set_clauses(project, subsection)
+        @clauses.each_with_index do |clause, m|
           #project clauses
-          csv << [subsection.full_code, '', clause.clause_code, clause.clausetitle.text]
-        end  
-      end      
-    end  
-    send_data @cadimage_keynote_export, :type => 'text/plain', :disposition => 'attachment; filename=#{filename}.txt'      
+          csv << [subsection.full_code, '', clause.caws_code, clause.clausetitle.text]
+        end
+      end
+    end
+
+    filename = "#{@project.code}_cadimage_keynote.csv"
+
+    send_data @cadimage_keynote, filename: filename, :type => 'text/csv'
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
       @project = Project.find(params[:id])
+    end
+
+    def set_sections(project)
+      if @project.CAWS?
+        @sections = Cawssection.project_sections(project)
+      else
+        ##
+      end
+    end
+
+    def set_subsections(project)
+      if @project.CAWS?
+        @subsections = Cawssubsection.project_subsections(project)
+      else
+        ##
+      end
+    end
+
+    def set_section_subsections(project, section)
+      if @project.CAWS?
+        @subsections = Cawssubsection.section_subsections(project, section)
+      else
+        ##
+      end
+    end
+
+    def set_clauses(project, subsection)
+      if @project.CAWS?
+        @clauses = Clause.subsection_clauses(project, subsection)
+      else
+        ##
+      end
     end
 
     def set_revision
