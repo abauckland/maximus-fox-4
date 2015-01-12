9c1cfa79cbf31af65ab1606018c179587a2a5f90
Andrew Burton <abauckland@gmail.com>
2015-01-12 20:55:07 +0000

    omit imageloader

--- app/models/print.rb
+++ app/models/print.rb
@@ -4,7 +4,7 @@ class Print < ActiveRecord::Base
   belongs_to :revision
   belongs_to :user
   
-  mount_uploader :issued, IssuedUploader
+#  mount_uploader :issued, IssuedUploader
 
   #Paperclip will also throw "Missing Required Validator Error" in case of PDF file upload. The workaround for that is: First install the "GhostScript" and then add "application/pdf" to content-type.
   
