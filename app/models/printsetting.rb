194ab9e4719818678c5e41d54874303852293dea
Andrew Burton <abauckland@gmail.com>
2015-02-07 13:44:26 +0000

    update print settings creation on project create

--- app/models/printsetting.rb
+++ app/models/printsetting.rb
@@ -3,34 +3,33 @@ class Printsetting < ActiveRecord::Base
   belongs_to :project
   
 
-after_create :set_default_values
+before_create :set_default_values
 
 def set_default_values
-  if new_record?
-    self.font_style ||= "helvetica"
-    self.font_size ||= "medium"
-    
-    self.structure ||= "revision by section"
-    self.prelim ||= "separate sections"
-    self.page_number ||= "by document"
-    
-    self.section_cover ||= "no cover"
-        
-    self.client_detail ||= "right"   
-    self.client_logo ||= "right" 
-    self.project_detail ||= "right" 
-    self.project_image ||= "right"     
-    self.company_detail ||= "right" 
-    
-    self.header_project ||= "show"
-    self.header_client ||= "show"  
-    self.header_document ||= "show"
-    self.header_logo ||= "do_not_show"
-    
-    self.footer_detail ||= "show"   
-    self.footer_author ||= "show"
-    self.footer_date ||= "show"
-  end
+    self.font_style = "helvetica"
+    self.font_size = "medium"
+
+    self.structure = "revision by section"
+    self.prelim = "separate sections"
+    self.page_number = "by document"
+
+    self.section_cover = "no cover"
+
+    self.client_detail = "right"
+    self.client_logo = "right" 
+    self.project_detail = "right" 
+    self.project_image = "right"
+    self.company_detail = "right" 
+
+    self.header_project = "show"
+    self.header_client = "show"  
+    self.header_document = "show"
+    self.header_logo = "none"
+
+    self.footer_detail = "show"
+    self.footer_author = "show"
+    self.footer_date = "show"
+
 end
 
 
