175a8a600945fde3df5cc22b8bedd940642d5a52
Andrew Burton <abauckland@gmail.com>
2015-02-07 15:38:34 +0000

    correct typo

--- db/migrate/20140511202747_create_performs.rb
+++ db/migrate/20140511202747_create_performs.rb
@@ -1,7 +1,7 @@
 class CreatePerforms < ActiveRecord::Migration
   def change
     create_table :performs do |t|
-      t.integer :performskey_id
+      t.integer :performkey_id
       t.integer :performvalue_id
 
       t.timestamps
