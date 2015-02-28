a424d1870383e62aee3c2b4833884e1e99aef98f
Andrew Burton <abauckland@gmail.com>
2015-02-28 14:08:40 +0000

    add bugsnag key to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,8 +10,8 @@ set :default_stage, "staging"
 
 set :rvm_type, :user
 
-#require "bugsnag/capistrano"
-#set :bugsnag_api_key, "4749e41c02e44cf6285fe8ea70c1cf46"
+require "bugsnag/capistrano"
+set :bugsnag_api_key, "7964370ce03759cccb398587aaccf03b"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
