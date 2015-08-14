e61adcd89108c4a445fc8a345ed1298640f4755e
Andrew Burton <abauckland@gmail.com>
2015-08-14 21:09:03 +0100

    general update

--- app/controllers/projectusers_controller.rb
+++ app/controllers/projectusers_controller.rb
@@ -1,49 +1,26 @@
 class ProjectusersController < ApplicationController
-#  before_filter :authenticate
-  before_action :set_project, only: [:index, :new ]
-  before_action :set_projectuser, only: [:edit, :update, :destroy]
 
+  before_action :set_project_user
+  before_action :set_project
+  before_action :set_subsections, only: [:new, :edit]
 
   layout "projects"
 
-  # GET /projects
-  # GET /projects.json
-  def index
 
-#    authorise_project_manager(params[:id])
-
+  def show
+    authorize :projectuser, :show?
     @projectusers = Projectuser.includes(:user, :project).where(:project_id => params[:id]).order(:role)
-
-#    user_ids = @projectusers.collect{|i| i.user_id}.uniq.sort   
-#    @company_users = User.where(:company_id => current_user.company_id).where.not(:id => user_ids)
-
-#    @projectuser = Projectuser.new 
-#    
-#    if @project.ref_system == "CAWS"
-#       @subsections = Cawssubsection.joins(:subsections => [:clauserefs => [:clauses => :speclines]]
-#                                    ).where('speclines.project_id' => @project.id
-#                                    )
-#    else
-##uniclass query here
-#    end
   end
-
 
   def edit
 
-#    authorise_project_manager(@projectuser.project_id)
-
-#    if @projectuser.project.ref_system == "CAWS"
-#       @subsections = Cawssubsection.joins(:subsections => [:clauserefs => [:clauses => :speclines]]
-#                                    ).where('speclines.project_id' => @projectuser.project_id
-#                                    )
-#    else
-##uniclass query here
-#    end
+#    @projectuser = Projectuser.find(params[:id])
+    set_subsections(@project)
   end
 
   def update
-#    @projectuser= Projectuser.find(params[:id])
+
+
     @projectuser.update(projectuser_params)
 
     respond_to do |format|
@@ -53,13 +30,13 @@ class ProjectusersController < ApplicationController
 
   def new
     @projectuser = Projectuser.new
+    set_subsections(@project)
   end
 
 
   # POST /projects
   # POST /projects.json
   def create
-
 
 #    project_id = params[:projectuser][:project_id]
 #    subsection_ids = params[:projectuser][:subsection_ids]
@@ -68,23 +45,7 @@ class ProjectusersController < ApplicationController
 
     @projectuser = Projectuser.new(projectuser_params)
 
-    #save project users - without subsectionuser recored for project user, user is able to access the whole project
     if @projectuser.save
-#      if subsection_ids
-#        if @projectuser.project.ref_system == "CAWS"
-
-#          subsection_ids.each do |subsection_id|
-#            subsection_ref = Subsection.where(:cawssubsection_id => subsection_id).first
-#            if subsection_ref
-#              @subsection = Subsectionuser.create(:subsection => subsection_ref.id, :projectuser => @projectuser.id)
-#            end
-#          end
-#        else
-##uniclass query here
-#        end
-#      end
-
-#    @project = Project.where(:id => @projectuser.project_id).first
 
       respond_to do |format| 
         format.html { redirect_to projectusers_path(:id => @projectuser.project_id) }
@@ -92,12 +53,8 @@ class ProjectusersController < ApplicationController
     end
   end
 
-def destroy
 
-#    @subsectionusers = Subsectionuser.where(:projectuser_id => @projectuser.id)
-#    @subsectionusers.each do |subsectionuser|
-#      subsectionuser.destory
-#    end
+def destroy
 
     @projectuser.destroy
 
@@ -110,25 +67,28 @@ end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_project_user
+      @projectuser = Projectuser.where(:project_id => params[:id], :user_id => current_user.id).first
+    end
+
+    def pundit_user
+      @projectuser
+    end
+
     def set_project
-      @project = Project.find(params[:id])
+#      set_projectuser
+      @project = Project.find(@projectuser.project_id)
     end
 
-    def set_projectuser
-      @projectuser = Projectuser.find(params[:id])
+    def set_subsections(project)
+       table_name = @project.ref_system.subsection_name
+       model = table_name.classify.constantize
+       @subsections = model.project_subsections(@project)
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+
     def projectuser_params
-      params.require(:projectuser).permit(:project_id, :user_id, :email, :role, :subsection_ids)
+      params.require(:projectuser).permit(:project_id, :user_id, :role, :date_from, :date_to, :subsection_ids => [])
     end
-
-#    def authorise_project_manager(project_id)
-#      #allow only manager of project to create new project users
-#      project_user = Projectuser.where(:user_id => current_user.id, :project_id => project_id , :role => "manage").first    
-#      if project_user.blank?
-#        redirect_to log_out_path
-#      end
-#    end
 
 end
