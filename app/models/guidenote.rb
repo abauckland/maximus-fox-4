f5a192c4c1637946c11d05db043e25d7422f300d
Andrew Burton <abauckland@gmail.com>
2015-09-12 12:27:45 +0100

    update validation field name

--- app/models/guidenote.rb
+++ app/models/guidenote.rb
@@ -3,7 +3,7 @@ class Guidenote < ActiveRecord::Base
 
   has_many :clauseguides
 
-  validates :description, presence: true,
+  validates :text, presence: true,
     uniqueness: {message: ": A guide with the same description already exists for the company" }
 
 end
