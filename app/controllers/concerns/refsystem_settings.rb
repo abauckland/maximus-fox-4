c74d9cb6f9993471b5324c0563f1debdec37546e
Andrew Burton <abauckland@gmail.com>
2015-09-12 12:29:24 +0100

    remove CAWS if filter - not needed at present

--- app/controllers/concerns/refsystem_settings.rb
+++ app/controllers/concerns/refsystem_settings.rb
@@ -8,12 +8,12 @@ module RefsystemSettings
 
   def section_names
 #TODO change ref system establishment
-    if @project.CAWS?
+#    if @project.CAWS?
       @subsection_name = 'cawssubsection'
       @subsection_model = @subsection_name.classify.constantize
       @section_name = 'cawssection'
       @section_model = @subsection_name.classify.constantize
-    end
+#    end
   end
 
 end
